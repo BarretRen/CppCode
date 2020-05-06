7efc4eee760169d5a274f4ca81ec555f92f80343
barret ren <barret.ren@outlook.com>
2020-05-06 23:15:45 +0800

    Cpp并发code

---
+++ CppConcurrency/1_helloworld.cpp
@@ -0,0 +1,18 @@
+﻿// CppConcurrency.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void hello()//子线程将要执行的函数入口
+{
+    cout << "hello concurrent world" << endl;
+}
+
+int main()
+{
+    thread th(hello);//创建一个子线程
+    th.join();
+}
