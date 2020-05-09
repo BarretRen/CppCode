0db3726fb15ad14eb71f65012e39f21dbdd70ed9
Barret Ren <barret.ren@outlook.com>
2020-05-09 11:00:47 +0800

    线程标识和最大线程数

---
+++ CppConcurrency/4_hardware_concurrency.cpp
@@ -0,0 +1,12 @@
+/*
+通过std::thread::hardware_concurrency()获取系统支持的最大线程数
+*/
+#include <iostream>
+#include <thread>
+
+int main4() {
+	unsigned int n = std::thread::hardware_concurrency();
+	std::cout << n << " concurrent threads are supported.\n";
+
+	return 0;
+}
