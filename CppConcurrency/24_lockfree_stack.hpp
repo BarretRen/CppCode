6497bb8910283824650df1b5209ec3154d90b5b9
barret ren <barret.ren@outlook.com>
2020-05-22 22:57:56 +0800

    fix delete node memory

--- CppConcurrency/24_lockfree_stack.hpp
+++ CppConcurrency/24_lockfree_stack.hpp
@@ -5,17 +5,6 @@
 template <typename T>
 class lock_free_stack
 {
-private:
-    struct node
-    {
-        std::shared_ptr<T> data; //使用共享指针保存数据
-        node *next;
-
-        node(const T &data) : data(std::make_shared(data)), next(nullptr) {}
-    };
-
-    std::atomic<node *> head; //原子操作类
-
 public:
     /*
     push操作：
@@ -48,6 +37,7 @@ public:
     */
     std::shared_ptr<T> pop()
     {
+        ++threads_in_pop;             //新线程调用pop，增加计数1
         node *old_head = head.load(); //先获得head，多线程等待，防止别的线程正在获取head
         /*
         使用“比较-交换”更新head：
@@ -57,7 +47,81 @@ public:
         */
         while (old_head && !head.compare_exchange_weak(old_head, old_head->next)) //old_head不能为空指针
             ;
-        return old_head ? old_head->data : std::shared_ptr<T>();//返回数据或空指针
-        //注意，old_head并没有从内存中删除，有内存泄漏
+        //此时的old_head为第一个有数据的节点，真正的head是它的next
+        std::shared_ptr<T> retVal;
+        if (old_head)
+        {
+            retVal.swap(old_head->data); //交换内容，将old_head的data传递给retVal
+        }
+        try_reclaim(old_head); //回收old_head节点，计数器减一
+        return retVal;
+    }
+
+private:
+    struct node
+    {
+        std::shared_ptr<T> data; //使用共享指针保存数据
+        node *next;
+
+        node(const T &data) : data(std::make_shared(data)), next(nullptr) {}
+    };
+
+    std::atomic<node *> head;             //原子操作类
+    std::atomic<unsigned> threads_in_pop; //原子计数，记录当前多少个线程在调用pop
+    std::atomic<node *> to_be_deleted;    //将要删除的节点列表，暂存，因为不能立刻删除，其他线程可能还在用
+
+    //释放node
+    static void delete_nodes(node *nodes)
+    {
+        while (nodes)
+        {
+            node *next = nodes->next;
+            delete nodes;
+            nodes = next;
+        }
+    }
+
+    void chain_pending_nodes(node *nodes)
+    {
+        node *last = nodes;
+        while (node *const next = last->next) // 9 让next指针指向链表的末尾
+        {
+            last = next;
+        }
+        chain_pending_nodes(nodes, last);
+    }
+
+    void chain_pending_nodes(node *first, node *last)
+    {
+        last->next = to_be_deleted;
+        while (!to_be_deleted.compare_exchange_weak(last->next, first)) // 用循环来保证last->next的正确性
+            ;
+    }
+    void chain_pending_node(node *n)
+    {
+        chain_pending_nodes(n, n);
+    }
+
+    void try_reclaim(node *old_head)
+    {
+        if (threads_in_pop == 1) // 当计数为1，表示只有当前线程正在调用pop函数，可以删除节点
+        {
+            // 返回to_be_deleted的第一个值值给左边，同时to_be_deleted赋值nullptr，因为to_be_deleted中可能有多个等待中的节点
+            node *nodes_to_delete = to_be_deleted.exchange(nullptr);
+            if (!--threads_in_pop) // 计数器减一，再次确认是否只有一个线程调用pop()
+            {
+                delete_nodes(nodes_to_delete); // 循环删除等待列表中的节点
+            }
+            else if (nodes_to_delete)
+            {
+                chain_pending_nodes(nodes_to_delete); // 6
+            }
+            delete old_head; // 删除新传入的节点
+        }
+        else
+        {
+            chain_pending_node(old_head); // 计数不是1，将节点添加到等待删除列表
+            --threads_in_pop;             //计数减一
+        }
     }
 };
